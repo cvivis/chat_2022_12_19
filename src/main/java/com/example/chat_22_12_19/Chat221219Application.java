0b554d6f871b002329857189a881c8a0e9acc81b
cvivis <hsy0754@naver.com>
2022-12-19 10:20:16 +0900

    gradle 변경

---
+++ src/main/java/com/example/chat_22_12_19/Chat221219Application.java
@@ -0,0 +1,13 @@
+package com.example.chat_22_12_19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat221219Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat221219Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chat_22_12_19/Chat221219ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_22_12_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat221219ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
